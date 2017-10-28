2c722c84caa5f6d7cc842f66747b06207bb1876b
zainab <zainab.hammami.zh@gmail.com>
2017-10-28 10:21:37 +0200

    fetch reviews to front end

--- kteer/src/app/profile/profile.component.ts
+++ kteer/src/app/profile/profile.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { CarouselConfig } from 'ngx-bootstrap/carousel';
+import {Router} from '@angular/router';
+import  { DataService } from './data.service.ts';
+
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css'],
-  providers: [{provide: CarouselConfig, useValue: {interval: 1500, noPause: true}}]
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.css'],
+	providers: []
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+	data: Object[] = [];
 
-  ngOnInit() {
-  }
+	constructor(private dataService: DataService) { }
+
+	ngOnInit() {
+		this.dataService.getReviews(localStorage.getItem("user-id")).subscribe(data => {
+			this.data = data;
+		})
+	}
 
 }
