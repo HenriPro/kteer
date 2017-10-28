5a4fc0da501ce9e521b619b4dc5df1a733a28897
Henri Pietila <henrismail@gmail.com>
2017-10-28 03:34:05 +0200

    added facebook auth

--- kteer/server/server.js
+++ kteer/server/server.js
@@ -1,15 +1,68 @@
-var express = require('express')
+var express = require('express');
 var mongoose = require('mongoose');
-var server = require('http').createServer(app);//
+var server = require('http').createServer(app);
+var morgan = require('morgan')
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser'); //unsure if needed, may remove
+var session = require('express-session')
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var config = require('./../secrets.js');
+
 var api = require('./api');
 
 var app = express();
 
+////////////////auth part 1/////////////////
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// config
+passport.use(new FacebookStrategy({
+  clientID: config.facebook.clientID,
+  clientSecret: config.facebook.clientSecret,
+  callbackURL: config.facebook.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
+
+
+//////////middleware//////////////
+app.use(morgan('tiny'));
+app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'LOLOLOLOLOLOL HAXORZZSS123' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(express.static(__dirname + '/../dist'));
+
+
+/////////////////auth/////////////////////////////////////
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'),
+  function (req, res) { });
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  function (req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 
 /////////////////////database//////////////////////////
@@ -18,13 +71,13 @@ mongoose.Promise = global.Promise;
 mongoose.connect(mongoURI);
 db = mongoose.connection;
 db.once('open', function () {
-	console.log('mongoDB is open');
+  console.log('mongoDB is open');
 });
 
 app.use('/api', api);
 
-app.get("*", ()=>{
-	//TODO
+app.get("*", () => {
+  //TODO
 })
 
 
